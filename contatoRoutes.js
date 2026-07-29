20fe7283dfd83b0b0971943f8429f979f13ec929
fabriciobuenob <fabricio.bueno@ifsc.edu.br>
2026-07-29 15:10:49 -0300

    Delete contatoRoutes.js

--- contatoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contatoController = require('../controllers/contatoController');
-router.get('/', contatoController.listar);
-router.post('/', contatoController.cadastrar);
-router.delete('/:id', contatoController.remover);
-module.exports = router;
